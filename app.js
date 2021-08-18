178a96dbdf865862d9cedfb5dc8e3b2a6d423cb4
Sunday <sunny.ajiroghene@gmail.com>
2021-08-18 21:01:00 +0100

    Printing my name with nodejs

---
+++ app.js
@@ -0,0 +1,8 @@
+
+let firstName = 'Sunday';
+let middleName = 'Onoriode';
+let lastName = 'AJIROGHENE';
+
+const print = console.log;
+
+print(`My names are: ${lastName}, ${middleName} ${firstName}`);
